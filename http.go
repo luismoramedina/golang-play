9fb7dfb193134b1ba964ed524f53dce454411927
luismoramedina <luismoramedina@gmail.com>
2016-10-18 18:12:08 +0200

    params in http request

--- http.go
+++ http.go
@@ -2,10 +2,21 @@ package main
 
 import "fmt"
 import "net/http"
+import "net/url"
 import "io/ioutil"
+import "encoding/json"
 
 func main() {
-   resp, error := http.Get("https://api.gopro.com/v2/channels/feed/playlists/photo-of-the-day.json?platform=web&page=1&per_page=9")
+
+   var myurl *url.URL
+   myurl, err := url.Parse("https://api.spotify.com/v1/search")
+   fmt.Println(err)
+   parameters := url.Values{}
+   parameters.Add("type", "track")
+   parameters.Add("q", "She Him Why Do You Let Me Stay Here")
+   myurl.RawQuery = parameters.Encode()
+
+   resp, error := http.Get(myurl.String())
    fmt.Println("error: ", error)
    if resp != nil {
       a, err := ioutil.ReadAll(resp.Body)
